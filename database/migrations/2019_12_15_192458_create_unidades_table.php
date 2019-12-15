4cc0124fa4afb79149769bc50797f3bb80779bfe
fran <navasfran98@gmail.com>
2019-12-15 13:27:42 -0600

    Migracion de unidades

---
+++ database/migrations/2019_12_15_192458_create_unidades_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUnidadesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unidades', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('titulo');
+            $table->text('descripcion')->nullable();
+            $table->unsignedBigInteger('materia_id');
+            $table->timestamps();
+            //relacion
+            $table->foreign('materia_id')->references('id')->on('materias');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('unidades');
+    }
+}
