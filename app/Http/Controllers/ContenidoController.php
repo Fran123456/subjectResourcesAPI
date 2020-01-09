1538fd0ba89f2a5c6058f7c9001cf71398447a25
Fran Navas <navasfran98@gmail.com>
2020-01-08 23:17:16 -0600

    requerimiento de inicio de sesion para los controladores

--- app/Http/Controllers/ContenidoController.php
+++ app/Http/Controllers/ContenidoController.php
@@ -16,6 +16,14 @@ class ContenidoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
+
+
     public function index()
     {
         //
--- app/Http/Controllers/UnidadController.php
+++ app/Http/Controllers/UnidadController.php
@@ -14,6 +14,11 @@ class UnidadController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
 
